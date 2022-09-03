ad298a71f920a08b3e870d565ca46936fd7b0333
C0deBr3ak3r <laurarodriguesprates17@gmail.com>
2022-09-03 18:30:08 -0300

    application goals

--- README.md
+++ README.md
@@ -11,3 +11,30 @@ A NeoVim integration with Spotify via Web API
 ```vim
 Plug 'https://github.com/C0deBr3ak3r/neofy.nvim'
 ```
+## Setup
+## Processes IDs
++ A001: User authentication
++ U001: Fetch user's playlist
++ U002: Fetch user's saved songs
++ U003: Fetch user's saved albuns
++ U004: Fetch user's saved shows
++ U005: Fetch user's saved episodes
+
+## Notes
+<p>
+Right now this is basically useless, the only part who fully functions as I planned is the error check. The authentication works but it can't access endpoints who requires user authentication, so because the application moves around of control users features through the Web API I must use another method of users authenticatio
+<br>  Yes, I was a little inattentive. Remember, always read documentations carefully
+<p>
+## TODO
++ Re-implement user authentication through Authentication Code Flow
++ Processes U001 and beyond
++ Display data in a buffer
++ Asynchronusly fetch songs/episodes in a playlist/shows
++ Make the code more readable and easier to implent new functionalities
++ Help page and documentation
+
+## Possibilities
++ Integrate with some status line plugins
++ Use icons such as [devicons](https://github.com/kyazdani42/nvim-web-devicons) to make the UI prettier (not sure if has a good icon for what I'm doing)
++ Port the help page and documentation for another language (probably portuguese)
+
--- lua/neofy.lua
+++ lua/neofy.lua
@@ -18,29 +18,58 @@ if (type(CLIENT_ID) == nil or type(CLIENT_SECRET) == nil) then
 	vim.api.nvim_err_writeln('NeoFy: client_id and/or client_secret are not defined , type :help neofy.nvim for more detais')
 end
 
--- Authenticate with the Spotify Web API
+-- Verifies if and error occurs in the call with API
+local function checkForAPIErrors(response, process_id)
+-- Parameters: response(table), process_id(string)
+-- Return: nil(if an error has occurred), response(if no error)
+-- Description: Verify if has an error in the response table and display an error message
 
+local error = response['error']
+
+	if (error) then
+		if response['error'] == 'invalid_client' then
+			vim.api.nvim_err_writeln('NeoFy: an error while authenticating has occurred'..
+			'\nNeoFy: error details: '..response['error_description']..
+			'\nNeoFy: check the GitHub page for some help')
+
+			return nil
+		end
+
+		vim.api.nvim_err_writeln('NeoFy: an error has occurred, process id: '..process_id..
+		'\nNeoFy: status code: '..error['status']..
+		'\n NeoFy: error details: '..error['message'])
+
+		return nil
+	end
+
+	return response
+
+end
+
+-- Authentication with Spotify Web API
 local ACCESS_TOKEN = nil
 
-local function authenticate (CLIENT_ID, CLIENT_SECRET)
+local function authenticate(CLIENT_ID, CLIENT_SECRET)
+--	Parameters: CLIENT_ID(string), CLIENT_SECRET(string)
+--	Return: nothing
+--	Description: Authenticate the user to the Spotify Web API and updates the access token
+
 	local client_credentials = base64.encode(CLIENT_ID..':'..CLIENT_SECRET)
 
 	-- Make the request for the access token
-	local response = io.popen(
+	local res_json = io.popen(
 		'curl -s -d grant_type=client_credentials '..
 		'-H \'Authorization: Basic '..client_credentials..'\' '..
 		'-H \'Content-Type: application/x-www-form-urlencoded\' '..
 		'-X POST https://accounts.spotify.com/api/token'
 	):read('a')
 
-	local parsed_response = json.decode(response)
+	-- Verifies if an error has occurred
+	local res_table = checkForAPIErrors(json.decode(res_json), 'A001')
 
-	-- Verifies if an error occurs
-	if (parsed_response['error'] ~= nil) then
-		vim.api.nvim_err_writeln('NeoFy: While authenticating an error has occurred\nNeoFy: error details: '..parsed_response['error_description']..'\nNeoFy: Check the setup guide on GitHub for some help')
-
+	if (res_table == nil) then
 		return
 	end
-	ACCESS_TOKEN = parsed_response['access_token']
-end
 
+	ACCESS_TOKEN = res_table['access_token']
+end
